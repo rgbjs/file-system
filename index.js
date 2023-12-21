8649ffbb389682d0178f22e53d4bd1f3f1160619
feiYu <946686638@qq.com>
2023-12-21 21:10:37 +0800

    更新日志记录

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { error, listen, webStaticResource } from './lib/init.js'
+import { error, listen, webStaticResource, routeError } from './lib/init.js'
 import http from 'http'
 import Koa from 'koa'
 import router from './router/index/index.js'
@@ -9,6 +9,7 @@ import path from 'path'
 const { project } = globalThis.config
 const app = new Koa()
 
+app.on('error', routeError)
 app.use(cors({ origin: project.cors }))
 app.use(koaStatic(webStaticResource.path)) // 前端静态资源
 app.use(router.routes(path.join())) // 路由
