23700642841cb6751cb09aa6d36a4cd325538c6b
feiYu <946686638@qq.com>
2023-12-27 00:39:54 +0800

    添加JSON解析库

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import http from 'http'
 import Koa from 'koa'
 import router from './router/index/index.js'
 import koaStatic from 'koa-static'
+import { bodyParser } from "@koa/bodyparser"
 import cors from './lib/cors.js'
 import path from 'path'
 
@@ -12,6 +13,7 @@ const app = new Koa()
 app.on('error', routeError)
 app.use(cors({ origin: project.cors }))
 app.use(koaStatic(webStaticResource.path)) // 前端静态资源
+app.use(bodyParser())
 app.use(router.routes(path.join())) // 路由
 
 const server = http.createServer(app.callback())
