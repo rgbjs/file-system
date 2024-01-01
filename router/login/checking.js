ed65b59d8886c9e22a5c287655741c9a6a8e01a2
feiYu <946686638@qq.com>
2024-01-01 16:51:46 +0800

    新增登录接口

---
+++ router/login/checking.js
@@ -0,0 +1,14 @@
+import { checkAccount, checkPassword } from '../../lib/rules.js'
+
+export const checkLogin = ({ account, password }) => {
+    if (!(checkAccount(account).code === 0 && checkPassword(password).code === 0)) {
+        return {
+            code: 1,
+            msg: '账号或密码有误'
+        }
+    }
+
+    return {
+        code: 0
+    }
+}
