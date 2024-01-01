e62c187944ba4f2d2898a749eebb8d21c692bc96
feiYu <946686638@qq.com>
2024-01-01 16:52:13 +0800

    注册路由

--- router/index/index.js
+++ router/index/index.js
@@ -1,8 +1,11 @@
 import Router from 'koa-router'
 import admin from '../admin/index.js'
+import user from '../user/index.js'
+import login from '../login/index.js'
+
 const router = new Router()
 const { api } = globalThis.config.project
 
-router.use(api, admin.routes())
+router.use(api, admin.routes(), user.routes(), login.routes())
 
 export default router
