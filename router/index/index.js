4c9dd0aad1c28bb464c984efadbd3c40fbdf6f2b
feiYu <946686638@qq.com>
2024-01-07 23:05:09 +0800

    增加文件路由模块

--- router/index/index.js
+++ router/index/index.js
@@ -2,10 +2,11 @@ import Router from 'koa-router'
 import admin from '../admin/index.js'
 import user from '../user/index.js'
 import login from '../login/index.js'
+import file from '../file/index.js'
 
 const router = new Router()
 const { api } = globalThis.config.project
 
-router.use(api, admin.routes(), user.routes(), login.routes())
+router.use(api, admin.routes(), user.routes(), login.routes(), file.routes())
 
 export default router
