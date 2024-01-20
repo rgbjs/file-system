81fc1b9216998992f985c084430189094b5b4a08
feiYu <946686638@qq.com>
2024-01-20 21:05:52 +0800

    [done] 导入其他模块

--- common/index.js
+++ common/index.js
@@ -1,2 +1,4 @@
 export *  as logger from './logger/index.js'
-export *  as checkAuthority from './checkAuthority/index.js'
+export { default as CheckAuthority } from './checkAuthority/index.js'
+export { default as randomKey } from './randomKey/index.js'
+export { default as randomStr } from './randomStr/index.js'
