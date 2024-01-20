2391fb7cafeb760bdfdf137b3e9262481c2db35c
feiYu <946686638@qq.com>
2024-01-20 21:15:46 +0800

    [done] 添加加密和验证加密模块

--- common/index.js
+++ common/index.js
@@ -1,4 +1,5 @@
 export *  as logger from './logger/index.js'
+export *  as encryp from './encryp/index.js'
 export { default as CheckAuthority } from './checkAuthority/index.js'
 export { default as randomKey } from './randomKey/index.js'
 export { default as randomStr } from './randomStr/index.js'
