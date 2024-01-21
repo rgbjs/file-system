dd5cac2399983c8c43c3802c01d7af149fd84f67
feiYu <946686638@qq.com>
2024-01-21 20:27:44 +0800

    [done] 配置开发环境

--- dev/index.js
+++ dev/index.js
@@ -2,17 +2,17 @@ import nodemon from 'nodemon'
 import path from 'path'
 import '#config'
 
-const { project, colorFn, ip } = globalThis.config
+const { project, color, ip } = globalThis.config
 const init = () => {
     console.clear()
     console.log('')
-    console.log(colorFn.success(project.success))
+    console.log(color.success(project.success))
     console.log('')
-    console.log(colorFn.success(`✨ http://127.0.0.1:${project.port}`))
+    console.log(color.success(`✨ http://127.0.0.1:${project.port}`))
     console.log('')
-    console.log(colorFn.success(`✨ http://${ip}:${project.port}`))
+    console.log(color.success(`✨ http://${ip}:${project.port}`))
     console.log('')
-    console.log(colorFn.success(`✨ 当前为开发模式, ${project.port} 端口监听中...`))
+    console.log(color.success(`✨ 当前为开发模式, ${project.port} 端口监听中...`))
     console.log('')
 }
 init()
@@ -32,6 +32,6 @@ nodemon.on('start', () => {
 nodemon.on('crash', () => {
     flag = true
     console.log('')
-    console.error(colorFn.danger('Error: 应用程序已崩溃...'))
+    console.error(color.danger('Error: 应用程序已崩溃...'))
     console.log('')
 })
